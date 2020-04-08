cbab96e1085f89bb0f079bd4a5f5be523e25bae9
Svetlana Zakalyukina <skrasn@mera.ru>
2020-04-08 18:24:50 +0300

    Initial Commit

---
+++ Pods/Target Support Files/Pods-Aware4Contact-Aware4ContactUITests/Pods-Aware4Contact-Aware4ContactUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Aware4Contact_Aware4ContactUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Aware4Contact_Aware4ContactUITests
+@end
---
+++ Pods/Target Support Files/Pods-Aware4Contact-Aware4ContactUITests/Pods-Aware4Contact-Aware4ContactUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Aware4Contact_Aware4ContactUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Aware4Contact_Aware4ContactUITestsVersionString[];
+
